2d4717ed5ddc974899e650681e365d582d7f1b31
Sohail Alakoozi <salakoozi@myseneca.ca>
2025-05-21 17:21:12 -0400

    Added lab2e

---
+++ lab2e.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+#Author: Sohail Alakoozi
+#Author ID: salakoozi
+#Date Created: 2025-05-21
+
+timer = 10
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+
+if timer == 0:
+    print('blast off!')

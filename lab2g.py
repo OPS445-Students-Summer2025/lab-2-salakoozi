86ffa3ab7137385a3dbf590bdf74e060f56220f8
Sohail Alakoozi <salakoozi@myseneca.ca>
2025-05-21 17:40:11 -0400

    Added lab2g

---
+++ lab2g.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+#Author: Sohail Alakoozi
+#Author ID: salakoozi
+#Date Created: 2025/05/21
+
+import sys
+
+if len(sys.argv) == 1:
+    timer = 3
+
+else: 
+    timer = int(sys.argv[1])
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+
+if timer == 0:
+    print('blast off!')
